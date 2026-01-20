ebd3bb6a84333f309bd563b46f3cd39dd9d6acbc
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2026-01-20 10:36:25 +0800

    更新 links.js

--- links.js
+++ links.js
@@ -39,7 +39,7 @@ const linkData = [
 { name: "grok", url: "https://grok.com/" },
       { name: "huggingface", url: "https://huggingface.co/chat/conversation/65d998e91f26106dad5ee807" },
       { name: "Zlib", url: "https://z-lib.hk/s/?languages%5B%5D=chinese&extensions%5B%5D=EPUB" },
-      { name: "gemini", url: "https://gemini.google.com/ },
+      { name: "gemini", url: "https://gemini.google.com/" },
       { name: "谷歌", url: "https://www.google.com/" },
       { name: "github", url: "https://github.com/" },
       { name: "gist", url: "https://gist.github.com/" },      
